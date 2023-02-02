63a766a48695c4ee00cde90a3a4f9438ccc7d4e5
Stu-PID <java4first@thelewins.net>
2023-02-01 19:18:49 -0500

    Remove WPILib Template Classes

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu-PID";
-    public static final String commitSHA = "f4df1fa";
-    public static final String timeStamp = "20230129-134837";
+    public static final String commitSHA = "4c3f192";
+    public static final String timeStamp = "20230201-191752";
 }
