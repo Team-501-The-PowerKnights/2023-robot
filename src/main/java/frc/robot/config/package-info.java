0280e4d97b13d877ab6e4efdab743196c25fde3f
Stu-PID <java4first@thelewins.net>
2023-02-03 21:52:04 -0500

    Normalize Formatting for 2023

--- src/main/java/frc/robot/config/package-info.java
+++ src/main/java/frc/robot/config/package-info.java
@@ -1,10 +1,10 @@
-/*-----------------------------------------------------------------------*/
-/* Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
-/* Open Source Software - may be modified and shared by other FRC teams  */
-/* under the terms of the Team501 license. The code must be accompanied  */
-/* by the Team 501 - The PowerKnights license file in the root directory */
-/* of this project.                                                      */
-/*-----------------------------------------------------------------------*/
+/*------------------------------------------------------------------------*/
+/*- Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/*- Open Source Software - may be modified and shared by other FRC teams  */
+/*- under the terms of the Team501 license. The code must be accompanied  */
+/*- by the Team 501 - The PowerKnights license file in the root directory */
+/*- of this project.                                                      */
+/*------------------------------------------------------------------------*/
 
 /**
  * This package contains the configuration files used to track the version
