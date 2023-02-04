7af6a6305ceda4df7f0a8be4ecb5c180174cdfbc
Stu-PID <java4first@thelewins.net>
2023-02-03 21:15:44 -0500

    Add preferences Package

---
+++ src/main/java/frc/robot/preferences/PreferencesManager.java
@@ -0,0 +1,82 @@
+/*-----------------------------------------------------------------------*/
+/* Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/* Open Source Software - may be modified and shared by other FRC teams  */
+/* under the terms of the Team501 license. The code must be accompanied  */
+/* by the Team 501 - The PowerKnights license file in the root directory */
+/* of this project.                                                      */
+/*-----------------------------------------------------------------------*/
+
+package frc.robot.preferences;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+// import frc.robot.subsystems.arm.ArmPreferences;
+// import frc.robot.subsystems.drive.DrivePreferences;
+// import frc.robot.subsystems.gripper.GripperPreferences;
+import frc.robot.telemetry.TelemetryNames;
+import frc.robot.utils.PKStatus;
+
+import riolog.PKLogger;
+import riolog.RioLogger;
+
+/**
+ * Add your docs here.
+ */
+public final class PreferencesManager {
+
+   /** Our classes' logger **/
+   private static final PKLogger logger = RioLogger.getLogger(PreferencesManager.class.getName());
+
+   private static PreferencesManager ourInstance;
+
+   private static String myName = "Prefs";
+
+   public static void constructInstance() {
+      SmartDashboard.putNumber(TelemetryNames.Properties.status, PKStatus.unknown.tlmValue);
+
+      if (ourInstance != null) {
+         throw new IllegalStateException(myName + " already constructed");
+      }
+
+      SmartDashboard.putNumber(TelemetryNames.Preferences.status, PKStatus.inProgress.tlmValue);
+
+      ourInstance = new PreferencesManager();
+
+      SmartDashboard.putNumber(TelemetryNames.Preferences.status, PKStatus.success.tlmValue);
+
+      logger.info("initialized");
+   }
+
+   public static PreferencesManager getInstance() {
+      if (ourInstance == null) {
+         throw new IllegalStateException(myName + " not constructed yet");
+      }
+      return ourInstance;
+   }
+
+   private PreferencesManager() {
+      logger.info("constructing");
+
+      // DrivePreferences.initialize();
+
+      // ArmPreferences.initialize();
+      // GripperPreferences.initialize();
+
+      logger.info("constructed");
+   }
+
+   public void logPreferences(PKLogger logger) {
+      StringBuilder buf = new StringBuilder();
+      buf.append(" preferences:");
+      for (String key : Preferences.getKeys().stream().collect(Collectors.toCollection(ArrayList::new))) {
+         buf.append("\n..."); // logger gobbles up leading spaces
+         buf.append(key).append(" = ").append(Preferences.getDouble(key, 3171960.));
+      }
+      logger.info(buf.toString());
+   }
+
+}
