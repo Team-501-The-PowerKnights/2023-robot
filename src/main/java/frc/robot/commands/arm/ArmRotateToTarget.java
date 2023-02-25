836d2ba35597f050c5fd8202223e0f111ba3eef3
Stu-PID <java4first@thelewins.net>
2023-02-25 16:20:42 -0500

    Add Joystick Tweak of Arm Rotation Position

---
+++ src/main/java/frc/robot/commands/arm/ArmRotateToTarget.java
@@ -0,0 +1,47 @@
+/*------------------------------------------------------------------------*/
+/*- Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/*- Open Source Software - may be modified and shared by other FRC teams  */
+/*- under the terms of the Team501 license. The code must be accompanied  */
+/*- by the Team 501 - The PowerKnights license file in the root directory */
+/*- of this project.                                                      */
+/*------------------------------------------------------------------------*/
+
+package frc.robot.commands.arm;
+
+import java.util.function.DoubleSupplier;
+
+import riolog.PKLogger;
+import riolog.RioLogger;
+
+public class ArmRotateToTarget extends ArmCommandBase {
+
+   /** Our classes' logger **/
+   private static final PKLogger logger = RioLogger.getLogger(ArmRotateToTarget.class.getName());
+
+   private final DoubleSupplier supplier;
+
+   public ArmRotateToTarget(DoubleSupplier supplier) {
+      logger.info("constructing {}", getName());
+
+      this.supplier = supplier;
+
+      logger.info("constructed");
+   }
+
+   @Override
+   protected void firstExecution() {
+      logger.trace("calling firstExecution()");
+
+   }
+
+   @Override
+   public void execute() {
+      double input = supplier.getAsDouble();
+      if (input == 0) {
+         return;
+      } else {
+         arm.rotateToTarget(input);
+      }
+   }
+
+}
