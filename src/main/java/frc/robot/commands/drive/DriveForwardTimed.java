96e59ac59f8a44252a7fa1ce3cb4b9560802e6f6
Stu-PID <java4first@thelewins.net>
2023-03-21 20:38:34 -0400

    Add Drive Timed Commands

---
+++ src/main/java/frc/robot/commands/drive/DriveForwardTimed.java
@@ -0,0 +1,110 @@
+/*-----------------------------------------------------------------------*/
+/* Copyright (c) Team 501 - The PowerKnights. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by other FRC teams */
+/* under the terms of the Team501 license. The code must be accompanied */
+/* by the Team 501 - The PowerKnights license file in the root directory */
+/* of this project. */
+/*-----------------------------------------------------------------------*/
+
+package frc.robot.commands.drive;
+
+import frc.robot.utils.TimerFromPeriod;
+
+import riolog.PKLogger;
+import riolog.RioLogger;
+
+/**
+ * Add your docs here.
+ */
+public class DriveForwardTimed extends DriveCommandBase {
+
+   /** Our classes' logger **/
+   private static final PKLogger logger = RioLogger.getLogger(DriveForwardTimed.class.getName());
+
+   // Duration to execute (in seconds)
+   private double duration;
+   // Speed to drive
+   private double speed = 0.30; // default
+   private final double turn = 0.0; // no turn component
+
+   // Timer to count it down during execute()
+   private TimerFromPeriod timer;
+
+   protected DriveForwardTimed() {
+      // Prevent direct instantiation
+   }
+
+   /**
+    * Creates an instance of a class to dive forward at the default
+    * speed.
+    * 
+    * @param duration
+    *           - duration to drive (seconds)
+    */
+   public DriveForwardTimed(double duration) {
+      logger.info("constructing {} for {} {}", getName(), duration, speed);
+
+      setValues(duration, speed);
+
+      logger.info("constructed");
+   }
+
+   /**
+    * Creates an instance of the class to drive forward at the specified
+    * speed.
+    * 
+    * @param duration
+    *           - duration to drive (seconds)
+    * @param speed
+    *           - speed to drive
+    */
+   public DriveForwardTimed(double duration, double speed) {
+      logger.info("constructing {} for {} {}", getName(), duration, speed);
+
+      setValues(speed, duration);
+
+      logger.info("constructed");
+   }
+
+   private void setValues(double duration, double speed) {
+      this.duration = duration;
+      if (speed < 0.0) {
+         speed = Math.abs(speed);
+      }
+      this.speed = speed;
+   }
+
+   @Override
+   public void initialize() {
+      super.initialize();
+
+      timer = new TimerFromPeriod(duration);
+   }
+
+   @Override
+   public void execute() {
+      super.execute();
+
+      timer.nextTic();
+   }
+
+   @Override
+   protected void firstExecution() {
+      logger.trace("drive.drive() called in firstExecution()");
+
+      drive.drive(speed, turn);
+   }
+
+   @Override
+   public boolean isFinished() {
+      return timer.isExpired();
+   }
+
+   @Override
+   public void end(boolean interrupted) {
+      super.end(interrupted);
+
+      drive.stop();
+   }
+
+}
---
+++ src/main/java/frc/robot/utils/TimerFromPeriod.java
@@ -0,0 +1,67 @@
+/*-----------------------------------------------------------------------*/
+/* Copyright (c) Team 501 - The PowerKnights. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by other FRC teams */
+/* under the terms of the Team501 license. The code must be accompanied */
+/* by the Team 501 - The PowerKnights license file in the root directory */
+/* of this project. */
+/*-----------------------------------------------------------------------*/
+
+package frc.robot.utils;
+
+import frc.robot.Robot;
+
+import riolog.PKLogger;
+import riolog.RioLogger;
+
+/**
+ * 
+ */
+public class TimerFromPeriod {
+
+   /** Our classes' logger **/
+   private static final PKLogger logger = RioLogger.getLogger(TimerFromPeriod.class.getName());
+
+   // Period retreived from robot (shouldn't ever change)
+   private static final double period;
+
+   static {
+      period = Robot.getLoopPeriod();
+   }
+
+   // Duration of timer (seconds)
+   @SuppressWarnings("unused")
+   private final double duration;
+   // Duration of timer (clicks)
+   private long count;
+
+   public TimerFromPeriod(double seconds) {
+      logger.info("constructing for {}", seconds);
+
+      duration = seconds;
+      count = secondsToClicks(seconds);
+      // logger.trace("duration = {}, count = {}", duration, count);
+
+      logger.info("constructed");
+   }
+
+   private long secondsToClicks(double seconds) {
+      return Double.valueOf(Math.ceil(seconds / period)).longValue();
+   }
+
+   /**
+    * 
+    * @return
+    */
+   public void nextTic() {
+      --count;
+   }
+
+   /**
+    * 
+    * @return
+    */
+   public boolean isExpired() {
+      return (count >= 0 ? false : true);
+   }
+
+}
