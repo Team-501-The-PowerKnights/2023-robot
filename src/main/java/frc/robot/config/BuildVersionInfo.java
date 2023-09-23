9c1c06477eca65d4a261bb43ab4b6bdcdc7bcae2
Stu-PID <java4first@thelewins.net>
2023-09-23 10:27:33 -0400

    Add Second LED String

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu-PID";
-    public static final String commitSHA = "19ee247";
-    public static final String timeStamp = "20230603-145749";
+    public static final String commitSHA = "7e840b3";
+    public static final String timeStamp = "20230923-102703";
 }
