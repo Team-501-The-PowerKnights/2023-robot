2d2e22e3bb8e133ab73688faab0b5ce00f9c3338
Stu-PID <java4first@thelewins.net>
2023-09-23 08:21:48 -0400

    Capture Real Robot Config Stuff

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu-PID";
-    public static final String commitSHA = "d6bad6e";
-    public static final String timeStamp = "20230919-043841";
+    public static final String commitSHA = "423fd21";
+    public static final String timeStamp = "20230919-194517";
 }
